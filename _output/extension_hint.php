c0ae2dc4e1c0003c5983968fa82c97149c0d53ea
Simon Hampel <simon@hampelgroup.com>
2020-08-28 12:06:31 +1000

    converting to using master branch for everything - remove _output from gitignore; remove _data; add _output

---
+++ _output/extension_hint.php
@@ -0,0 +1,14 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE CLASS EXTENSIONS IN THE CONTROL PANEL.
+
+namespace Hampel\LogDigest\XF\Admin\Controller
+{
+	class XFCP_Tools extends \XF\Admin\Controller\Tools {}
+}
+
+namespace Hampel\LogDigest\XF\Repository
+{
+	class XFCP_ErrorLog extends \XF\Repository\ErrorLog {}
+}
